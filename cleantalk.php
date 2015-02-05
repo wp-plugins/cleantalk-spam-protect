7d8b01f7e61a44a3f75b5147f5d64e94933cb5ac
znaeff <znaeff@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-05 11:42:36 +0000

    4.16 ready
    git-svn-id: https://plugins.svn.wordpress.org/cleantalk-spam-protect/trunk@1083042 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cleantalk.php
+++ cleantalk.php
@@ -10,7 +10,7 @@
 
 define('CLEANTALK_PLUGIN_DIR', plugin_dir_path(__FILE__));
 
-$ct_agent_version = 'wordpress-415';
+$ct_agent_version = 'wordpress-416';
 $ct_plugin_name = 'Anti-spam by CleanTalk';
 $ct_checkjs_frm = 'ct_checkjs_frm';
 $ct_checkjs_register_form = 'ct_checkjs_register_form';
