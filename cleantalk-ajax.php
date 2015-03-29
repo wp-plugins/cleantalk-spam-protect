5bcc4ae90a1b8662d457bdbb96fe5f58efed2bc0
Vlad Cleantalk <Vlad Cleantalk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-29 18:54:21 +0000

    small fixes in 5.2
    git-svn-id: https://plugins.svn.wordpress.org/cleantalk-spam-protect/tags/5.2@1123201 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cleantalk-ajax.php
+++ cleantalk-ajax.php
@@ -11,13 +11,18 @@ add_action( 'wp_ajax_validate_email', 'ct_validate_email_ajaxlogin',1 );
 /*hooks for user registration*/
 add_action( 'user_register', 'ct_user_register_ajaxlogin',1 );
 
+/*hooks for WPUF pro */
+add_action( 'wp_ajax_nopriv_wpuf_submit_register', 'ct_wpuf_submit_register',1 );
+add_action( 'wp_ajax_wpuf_submit_register', 'ct_wpuf_submit_register',1 );
+
 
 function ct_validate_email_ajaxlogin($email=null, $is_ajax=true)
 {
 	require_once(CLEANTALK_PLUGIN_DIR . 'cleantalk-public.php');
-	global $ct_agent_version, $ct_checkjs_register_form, $ct_session_request_id_label, $ct_session_register_ok_label, $bp, $ct_signup_done, $ct_formtime_label, $ct_negative_comment, $ct_options;
+	global $ct_agent_version, $ct_checkjs_register_form, $ct_session_request_id_label, $ct_session_register_ok_label, $bp, $ct_signup_done, $ct_formtime_label, $ct_negative_comment, $ct_options, $ct_data;
 	
 	$ct_options=ct_get_options();
+	$ct_data=ct_get_data();
 	
 	$email = is_null( $email ) ? $email : $_POST['email'];
 	$email=sanitize_email($email);
@@ -109,9 +114,10 @@ function ct_validate_email_ajaxlogin($email=null, $is_ajax=true)
 function ct_user_register_ajaxlogin($user_id)
 {
 	require_once(CLEANTALK_PLUGIN_DIR . 'cleantalk-public.php');
-	global $ct_agent_version, $ct_checkjs_register_form, $ct_session_request_id_label, $ct_session_register_ok_label, $bp, $ct_signup_done, $ct_formtime_label, $ct_negative_comment, $ct_options;
+	global $ct_agent_version, $ct_checkjs_register_form, $ct_session_request_id_label, $ct_session_register_ok_label, $bp, $ct_signup_done, $ct_formtime_label, $ct_negative_comment, $ct_options, $ct_data;
 	
 	$ct_options=ct_get_options();
+	$ct_data=ct_get_data();
 
 	if(class_exists('AjaxLogin')&&isset($_POST['action'])&&$_POST['action']=='register_submit')
 	{
@@ -170,6 +176,64 @@ function ct_user_register_ajaxlogin($user_id)
 		}
 	}
 	return $user_id;
+}
+
+function ct_wpuf_submit_register()
+{
+	require_once(CLEANTALK_PLUGIN_DIR . 'cleantalk-public.php');
+	global $ct_agent_version, $ct_checkjs_register_form, $ct_session_request_id_label, $ct_session_register_ok_label, $bp, $ct_signup_done, $ct_formtime_label, $ct_negative_comment, $ct_options, $ct_data;
+	
+	$ct_data=ct_get_data();
+	
+	$ct_options=ct_get_options();
+	
+	$sender_email = null;
+    $message = '';
+    
+    foreach ($_POST as $key => $value)
+    {
+    	if ($sender_email === null && preg_match("/^\S+@\S+\.\S+$/", $value))
+    	{
+            $sender_email = $value;
+        }
+        else
+        {
+        	$message.="$value\n";
+        }
+    }
+    
+	if($sender_email!=null)
+	{
+		$checkjs = js_test('ct_checkjs', $_COOKIE, true);
+		$submit_time = submit_time_test();
+	    $sender_info = get_sender_info();
+	    $sender_info['post_checkjs_passed']=$checkjs;
+	    
+		$sender_info = json_encode($sender_info);
+		if ($sender_info === false)
+		{
+			$sender_info= '';
+		}
+		
+		$ct_base_call_result = ct_base_call(array(
+			'message' => $message,
+			'example' => null,
+			'sender_email' => $sender_email,
+			'sender_nickname' => null,
+			'sender_info' => $sender_info,
+			'post_info'=>null,
+			'checkjs' => $checkjs));
+		
+		$ct = $ct_base_call_result['ct'];
+		$ct_result = $ct_base_call_result['ct_result'];
+		if ($ct_result->allow == 0)
+		{
+			$result=Array('success'=>false,'error'=>$ct_result->comment);
+			@header( 'Content-Type: application/json; charset=' . get_option( 'blog_charset' ) );
+			print json_encode($result);
+			die();
+		}
+	}
 }
 
 ?>
