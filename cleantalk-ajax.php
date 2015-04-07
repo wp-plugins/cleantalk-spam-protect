d789a398eaf0073b5b1dec7d09d85f2173c46dda
Vlad Cleantalk <Vlad Cleantalk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-07 20:15:50 +0000

    added compatibility with some ajax forms
    git-svn-id: https://plugins.svn.wordpress.org/cleantalk-spam-protect/trunk@1129875 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cleantalk-ajax.php
+++ cleantalk-ajax.php
@@ -23,6 +23,20 @@ add_action( 'wp_ajax_mymail_form_submit', 'ct_mymail_form_submit',1 );
 add_action( 'wp_ajax_nopriv_wysija_ajax', 'ct_wysija_ajax',1 );
 add_action( 'wp_ajax_wysija_ajax', 'ct_wysija_ajax',1 );
 
+/*hooks for cs_registration_validation */
+add_action( 'wp_ajax_nopriv_cs_registration_validation', 'ct_cs_registration_validation',1 );
+add_action( 'wp_ajax_cs_registration_validation', 'ct_cs_registration_validation',1 );
+
+/*hooks for cs_registration_validation */
+add_action( 'wp_ajax_nopriv_cs_registration_validation', 'ct_cs_registration_validation',1 );
+add_action( 'wp_ajax_cs_registration_validation', 'ct_cs_registration_validation',1 );
+
+/*hooks for send_message and request_appointment */
+add_action( 'wp_ajax_nopriv_send_message', 'ct_sm_ra',1 );
+add_action( 'wp_ajax_send_message', 'ct_sm_ra',1 );
+add_action( 'wp_ajax_nopriv_request_appointment', 'ct_sm_ra',1 );
+add_action( 'wp_ajax_request_appointment', 'ct_sm_ra',1 );
+
 function ct_validate_email_ajaxlogin($email=null, $is_ajax=true)
 {
 	require_once(CLEANTALK_PLUGIN_DIR . 'cleantalk-public.php');
@@ -339,6 +353,102 @@ function ct_wysija_ajax()
 		}
 	}
 }
+
+function ct_cs_registration_validation()
+{
+	
+	require_once(CLEANTALK_PLUGIN_DIR . 'cleantalk-public.php');
+	global $ct_agent_version, $ct_checkjs_register_form, $ct_session_request_id_label, $ct_session_register_ok_label, $bp, $ct_signup_done, $ct_formtime_label, $ct_negative_comment, $ct_options, $ct_data;
+	
+	$ct_data=ct_get_data();
+	
+	$ct_options=ct_get_options();
+	
+	$sender_email = null;
+    $message = '';
+    
+    ct_get_fields($sender_email,$message,$_POST);
+    
+	if($sender_email!=null)
+	{
+		$checkjs = js_test('ct_checkjs', $_COOKIE, true);
+		$submit_time = submit_time_test();
+	    $sender_info = get_sender_info();
+	    $sender_info['post_checkjs_passed']=$checkjs;
+	    
+		$sender_info = json_encode($sender_info);
+		if ($sender_info === false)
+		{
+			$sender_info= '';
+		}
+		
+		$ct_base_call_result = ct_base_call(array(
+			'message' => $message,
+			'example' => null,
+			'sender_email' => $sender_email,
+			'sender_nickname' => null,
+			'sender_info' => $sender_info,
+			'post_info'=>null,
+			'checkjs' => $checkjs));
+		
+		$ct = $ct_base_call_result['ct'];
+		$ct_result = $ct_base_call_result['ct_result'];
+		if ($ct_result->allow == 0)
+		{
+			$result=Array("type"=>"error","message"=>$ct_result->comment);
+			print json_encode($result);
+			die();
+		}
+	}
+}
+
+function ct_sm_ra()
+{
+	require_once(CLEANTALK_PLUGIN_DIR . 'cleantalk-public.php');
+	global $ct_agent_version, $ct_checkjs_register_form, $ct_session_request_id_label, $ct_session_register_ok_label, $bp, $ct_signup_done, $ct_formtime_label, $ct_negative_comment, $ct_options, $ct_data;
+	
+	$ct_data=ct_get_data();
+	
+	$ct_options=ct_get_options();
+	
+	$sender_email = null;
+    $message = '';
+    
+    ct_get_fields($sender_email,$message,$_POST);
+    
+    
+	if($sender_email!=null)
+	{
+		$checkjs = js_test('ct_checkjs', $_COOKIE, true);
+		$submit_time = submit_time_test();
+	    $sender_info = get_sender_info();
+	    $sender_info['post_checkjs_passed']=$checkjs;
+	    
+		$sender_info = json_encode($sender_info);
+		if ($sender_info === false)
+		{
+			$sender_info= '';
+		}
+		
+		$ct_base_call_result = ct_base_call(array(
+			'message' => $message,
+			'example' => null,
+			'sender_email' => $sender_email,
+			'sender_nickname' => null,
+			'sender_info' => $sender_info,
+			'post_info'=>null,
+			'checkjs' => $checkjs));
+		
+		$ct = $ct_base_call_result['ct'];
+		$ct_result = $ct_base_call_result['ct_result'];
+		if ($ct_result->allow == 0)
+		{
+			print $ct_result->comment;
+			die();
+		}
+	}
+}
+
 
 function ct_get_fields(&$email,&$message,$arr)
 {
