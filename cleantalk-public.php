bf34bd15adb6beaa11fa9931276f2831c138a669
Vlad Cleantalk <Vlad Cleantalk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-05 16:04:47 +0000

    fix for digital downloads
    git-svn-id: https://plugins.svn.wordpress.org/cleantalk-spam-protect/trunk@1175419 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- cleantalk-public.php
+++ cleantalk-public.php
@@ -1533,7 +1533,8 @@ function ct_contact_form_validate_postdata () {
         (isset($_POST['log']) && isset($_POST['pwd']) && isset($pagenow) && $pagenow == 'wp-login.php') || // WordPress log in form
         (isset($pagenow) && $pagenow == 'wp-login.php' && isset($_GET['action']) && $_GET['action']=='lostpassword')||
         ct_check_array_keys($_POST) ||
-        (isset($pagenow) && $pagenow == 'wp-cron.php')
+        (isset($pagenow) && $pagenow == 'wp-cron.php' ||
+        strpos($_SERVER['REQUEST_URI'],'/checkout/')!==false)
         ) {
         return null;
     }
